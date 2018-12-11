6436c9a8f96e726648aad9755752a5ad37d7d83c
Carl Björk <carl@carlbjork.com>
2018-12-11 11:34:18 +0100

    Initial Commit

---
+++ uDator_LAB5_T5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  uDator_LAB5_T5
+//
+//  Created by Carl Björk on 2018-12-11.
+//  Copyright © 2018 Carl Björk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
